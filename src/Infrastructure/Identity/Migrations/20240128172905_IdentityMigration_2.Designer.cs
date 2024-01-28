828487b223d4a610a8efdd2df4c670b92c420402
Айнур Якупов <yak.ainur@yandex.ru>
2024-01-28 23:45:33 +0300

    update migaration identity

--- src/Infrastructure/Identity/Migrations/20240128172905_IdentityMigration_2.Designer.cs
+++ src/Infrastructure/Identity/Migrations/20240128172905_IdentityMigration_2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Identity.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Identity.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20240128172905_IdentityMigration_2")]
+    partial class IdentityMigration_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Identity/Migrations/20240128172905_IdentityMigration_2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityMigration_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_identity_tokens_ip_address",
+                table: "identity_tokens");
+
+            migrationBuilder.UpdateData(
+                table: "identity_tokens",
+                keyColumn: "id",
+                keyValue: 1L,
+                column: "expiration",
+                value: new DateTime(2024, 2, 4, 20, 29, 5, 531, DateTimeKind.Local).AddTicks(6675));
+
+            migrationBuilder.UpdateData(
+                table: "identity_tokens",
+                keyColumn: "id",
+                keyValue: 2L,
+                column: "expiration",
+                value: new DateTime(2024, 2, 4, 20, 29, 5, 531, DateTimeKind.Local).AddTicks(6713));
+
+            migrationBuilder.UpdateData(
+                table: "identity_users",
+                keyColumn: "id",
+                keyValue: 1L,
+                column: "date_created",
+                value: new DateTimeOffset(new DateTime(2024, 1, 28, 17, 29, 5, 531, DateTimeKind.Unspecified).AddTicks(2103), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "identity_users",
+                keyColumn: "id",
+                keyValue: 2L,
+                column: "date_created",
+                value: new DateTimeOffset(new DateTime(2024, 1, 28, 17, 29, 5, 531, DateTimeKind.Unspecified).AddTicks(2107), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.CreateIndex(
+                name: "ix_identity_tokens_ip_address",
+                table: "identity_tokens",
+                column: "ip_address");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_identity_tokens_ip_address",
+                table: "identity_tokens");
+
+            migrationBuilder.UpdateData(
+                table: "identity_tokens",
+                keyColumn: "id",
+                keyValue: 1L,
+                column: "expiration",
+                value: new DateTime(2023, 9, 3, 10, 47, 25, 636, DateTimeKind.Local).AddTicks(3506));
+
+            migrationBuilder.UpdateData(
+                table: "identity_tokens",
+                keyColumn: "id",
+                keyValue: 2L,
+                column: "expiration",
+                value: new DateTime(2023, 9, 3, 10, 47, 25, 636, DateTimeKind.Local).AddTicks(3546));
+
+            migrationBuilder.UpdateData(
+                table: "identity_users",
+                keyColumn: "id",
+                keyValue: 1L,
+                column: "date_created",
+                value: new DateTimeOffset(new DateTime(2023, 8, 27, 7, 47, 25, 635, DateTimeKind.Unspecified).AddTicks(5145), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "identity_users",
+                keyColumn: "id",
+                keyValue: 2L,
+                column: "date_created",
+                value: new DateTimeOffset(new DateTime(2023, 8, 27, 7, 47, 25, 635, DateTimeKind.Unspecified).AddTicks(5151), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.CreateIndex(
+                name: "ix_identity_tokens_ip_address",
+                table: "identity_tokens",
+                column: "ip_address",
+                unique: true);
+        }
+    }
+}
